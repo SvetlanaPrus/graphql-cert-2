689471c3e93cb108a8df72e7adf62a16f805226d
Svetlana Prus <swetlana.prus@gmail.com>
2022-04-13 14:13:52 +0200

    2.6 Implementing Query Resolvers

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,8 +1,19 @@
 const resolvers = {
-    Query: {
+    //Query: {
         // returns an array of Tracks that will be used to populate
         // the homepage grid of our web client
-        tracksForHome: (parent, args, context, info) => {},
+        //tracksForHome: (parent, args, context, info) => {},
+    //}
+    Query: {
+        // get all tracks, will be used to populate the homepage grid of our web client
+        tracksForHome: (_, __, {dataSources}) => {
+            return dataSources.trackAPI.getTracksForHome();
+        }
+    },
+    Track: {
+        author: ({authorId}, _, {dataSources}) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        }
     }
 };
 
